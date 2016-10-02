12c16e209c9997330d5c2e56dabe9eebc9b617bd
Lisa Cook <jemilox@gmail.com>
2016-10-02 10:00:39 -0500

    stylesheet linked angular routers working

---
+++ public/scripts/controllers/homeController.js
@@ -0,0 +1,3 @@
+myApp.controller('addJobController', ['$scope', '$http', function ($scope, $http) {
+  console.log('in addJobController');
+}]);
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,23 @@
 console.log('sourced');
+
+var myApp = angular.module('myApp', ['ngRoute']);
+
+//home controller
+myApp.controller('home', ['$scope', '$http', function ($scope, $http) {
+  console.log('in home controller');
+}]);
+
+myApp.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.
+  when ('/addjob', {
+    templateUrl: "views/addjobview.html",
+    controller: "addJobController"
+  }).
+  when ('/home', {
+    templateUrl: "views/home.html",
+    controller: "homeController"
+  }).
+  otherwise({
+    redirectTo: "/home"
+  });
+}]);
