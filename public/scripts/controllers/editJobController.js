492984858c55fa97b7448633b0fd96a948179c1a
Lisa Cook <jemilox@gmail.com>
2016-10-04 16:04:15 -0500

    delete in edit view works

--- public/scripts/controllers/editJobController.js
+++ public/scripts/controllers/editJobController.js
@@ -3,8 +3,10 @@ myApp.constant('moment', moment);
 
 myApp.controller('editJobController', ['$scope', '$http', 'moment', 'factory', function ($scope, $http, moment, factory){
   console.log('in editJobController');
-
+  //this is the job that was selected from another view and received from factory
   var jobId = factory.currentJobId();
+  //save for other functions that need to change the job
+  $scope.id = '';
 
   //set ng-show to false on each
   $scope.editCo = false;
@@ -15,6 +17,7 @@ myApp.controller('editJobController', ['$scope', '$http', 'moment', 'factory', f
 
   //get all from factory find job from id that was clicked
   factory.getAll().then(function (results) {
+    //save get from factory into allJobs
     $scope.allJobs = results.data;
     console.log('in edit getAll results', $scope.allJobs);
     for (var i = 0; i < $scope.allJobs.length; i++) {
@@ -22,6 +25,8 @@ myApp.controller('editJobController', ['$scope', '$http', 'moment', 'factory', f
       //console.log($scope.allJobs[i].id);
       //console.log(jobId);
       if ($scope.allJobs[i].id === jobId){
+        //save parts of the job to display on dom
+        $scope.id = $scope.allJobs[i].id;
         $scope.company = $scope.allJobs[i].company;
         $scope.pieces = $scope.allJobs[i].pieces;
         $scope.duedate = moment($scope.allJobs[i].duedate).format('M/D/YY');
@@ -34,10 +39,12 @@ myApp.controller('editJobController', ['$scope', '$http', 'moment', 'factory', f
     console.log('in editButtonsCo');
     $scope.editCo = true;
   };
+  //show edit fields if clicked
   $scope.editButtonsPi = function () {
     console.log('in editButtonsPi');
     $scope.editPi = true;
   };
+  //show edit fields if clicked
   $scope.editButtonsDate = function () {
     console.log('in editButtonsDate');
     $scope.editD = true;
@@ -48,6 +55,7 @@ myApp.controller('editJobController', ['$scope', '$http', 'moment', 'factory', f
 
     factory.editPieces($scope.editPiecesmodel).then(function (results) {
       console.log('made it back from edit');
+      $scope.editPi = false;
     });
 
   };
@@ -57,6 +65,8 @@ myApp.controller('editJobController', ['$scope', '$http', 'moment', 'factory', f
 
     factory.editCompany($scope.editCompanymodel).then(function (results) {
       console.log('made it back from edit');
+      $scope.editCo = false;
+      $scope.$apply();
     });
 
   };
@@ -66,8 +76,20 @@ myApp.controller('editJobController', ['$scope', '$http', 'moment', 'factory', f
 
     factory.editDate($scope.editDueDatemodel).then(function (results) {
       console.log('made it back from edit');
+      $scope.editD = false;
+    
     });
 
   };//end editDate
+
+  $scope.deleteJob = function () {
+    console.log('in delete', $scope.id);
+    var objectToSend = {
+      id: $scope.id
+    };
+    factory.deletejob(objectToSend).then(function (response) {
+      console.log('made it to then');
+    });
+  };
 
 }]);//end controller
