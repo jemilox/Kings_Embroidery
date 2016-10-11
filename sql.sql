881b388a7fd3f3952c30ea270fafa4c0fc0f520e
Lisa Cook <jemilox@gmail.com>
2016-10-10 19:47:14 -0500

    everything works starting employee table

--- sql.sql
+++ sql.sql
@@ -1,3 +1,10 @@
+
+CREATE TABLE employees (
+	id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+	name VARCHAR(40)
+	);
+
+
 CREATE TABLE jobs (
 	id SERIAL PRIMARY KEY UNIQUE NOT NULL,
 	company VARCHAR(30),
@@ -5,13 +12,21 @@ CREATE TABLE jobs (
 	pieces INT,
 	complete BOOLEAN,
 	harddate BOOLEAN,
-	notes VARCHAR(200)
+	notes VARCHAR(200),
+	employeeid INT REFERENCES employees (id)
 	);
 
 
+	INSERT INTO employees (name) VALUES ('Cindy');
+	INSERT INTO employees (name) VALUES ('Marsha');
+	INSERT INTO employees (name) VALUES ('Matt');
+	INSERT INTO employees (name) VALUES ('Tyler');
+	INSERT INTO employees (name) VALUES ('Lora');
+	INSERT INTO employees (name) VALUES ('Carrie');
+	INSERT INTO employees (name) VALUES ('Teresa');
+	INSERT INTO employees (name) VALUES ('Will');
 
-
-	INSERT INTO jobs (company, duedate, pieces, complete, harddate, notes) VALUES ('MEOW', '10/17/16', 76, false, true, 'Dont forget...');
+ INSERT INTO jobs (company, duedate, pieces, complete, harddate, notes) VALUES ('MEOW', '10/17/16', 76, false, true, 'Dont forget...');
 		INSERT INTO jobs (company, duedate, pieces, complete, harddate, notes) VALUES ('MEOW', '10/17/16', 76, true, false, 'Dont forget...');
 		INSERT INTO jobs (company, duedate, pieces, complete, harddate, notes) VALUES ('MEOW', '10/18/16', 76, true, true, 'Dont forget...');
 		INSERT INTO jobs (company, duedate, pieces, complete, harddate, notes) VALUES ('MEOW', '10/18/16', 76, false, true, 'Dont forget...');
