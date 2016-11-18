f2017483975d2b060a38c1e94b5f2f98d9556040
Lisa Cook <jemilox@gmail.com>
2016-11-18 00:02:18 -0600

    previous and future weeks work

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -4,25 +4,10 @@ myApp.constant('moment', moment);
 var socket = io();
 
 myApp.controller('homeController', ['$scope', '$http', 'moment', 'factory', '$q', function ($scope, $http, moment, factory, $q){
-  console.log('in homeController', moment().day(-13).format('M/D/YY'));
+
   //status 0 means current week, negative is previous, positive is future
   var status = 0;
 
-
-
-  $scope.changeWeekStatus = function (number) {
-    console.log('in changeWeekStatus click');
-    status = status + number;
-    if (status < 0 ){
-      console.log('status is negative', status);
-    } else if (status === 0){
-      console.log('status is now', status);
-    } else {
-      console.log('status is positive', status);
-    }
-  };
-
-  $scope.dropDownContent = false;
 
   $scope.dateMonOne = moment().day(1).format('M/D/YY');
   $scope.dateTuesOne = moment().day(2).format('M/D/YY');
@@ -34,6 +19,38 @@ myApp.controller('homeController', ['$scope', '$http', 'moment', 'factory', '$q'
   $scope.dateWedTwo = moment().day(10).format('M/D/YY');
   $scope.dateThursTwo = moment().day(11).format('M/D/YY');
   $scope.dateFriTwo = moment().day(12).format('M/D/YY');
+
+  $scope.changeWeekStatus = function (number) {
+    console.log('in changeWeekStatus click');
+    status = status + number;
+    if (status !== 0){
+      console.log('status is negative', status);
+      $scope.dateMonOne = moment().day(1 + status * 14).format('M/D/YY');
+      $scope.dateTuesOne = moment().day(2 + status * 14).format('M/D/YY');
+      $scope.dateWedOne = moment().day(3 + status * 14).format('M/D/YY');
+      $scope.dateThursOne = moment().day(4 + status * 14).format('M/D/YY');
+      $scope.dateFriOne = moment().day(5 + status * 14).format('M/D/YY');
+      $scope.dateMonTwo = moment().day(8 + status * 14).format('M/D/YY');
+      $scope.dateTuesTwo = moment().day(9 + status * 14).format('M/D/YY');
+      $scope.dateWedTwo = moment().day(10 + status * 14).format('M/D/YY');
+      $scope.dateThursTwo = moment().day(11 + status * 14).format('M/D/YY');
+      $scope.dateFriTwo = moment().day(12 + status * 14).format('M/D/YY');
+    } else {
+      console.log('status is now', status);
+      $scope.dateMonOne = moment().day(1).format('M/D/YY');
+      $scope.dateTuesOne = moment().day(2).format('M/D/YY');
+      $scope.dateWedOne = moment().day(3).format('M/D/YY');
+      $scope.dateThursOne = moment().day(4).format('M/D/YY');
+      $scope.dateFriOne = moment().day(5).format('M/D/YY');
+      $scope.dateMonTwo = moment().day(8).format('M/D/YY');
+      $scope.dateTuesTwo = moment().day(9).format('M/D/YY');
+      $scope.dateWedTwo = moment().day(10).format('M/D/YY');
+      $scope.dateThursTwo = moment().day(11).format('M/D/YY');
+      $scope.dateFriTwo = moment().day(12).format('M/D/YY');
+    }
+    $scope.getAll();
+  };
+
 
   $scope.futureJobs = [];
   $scope.alljobs = [];
@@ -50,23 +67,24 @@ myApp.controller('homeController', ['$scope', '$http', 'moment', 'factory', '$q'
 
   $scope.pastJobsfunction = function (thisJob) {
     //console.log('in past jobs function', thisJob);
-    var thisJobId = thisJob.id;
-    var parsed = Date.parse(thisJob.duedate);
-    var beforeMonday = Date.parse($scope.dateMonOne);
-    if (!thisJob.complete){
-      if(parsed < beforeMonday){
-        factory.changeCurrentJobId(thisJobId);
-        factory.editDate($scope.dateMonOne).then(function () {
-          $scope.monOneJobs.push(thisJob);
-        });
+    if(status === 0){
+      var thisJobId = thisJob.id;
+      var parsed = Date.parse(thisJob.duedate);
+      var beforeMonday = Date.parse($scope.dateMonOne);
+      if (!thisJob.complete){
+        if(parsed < beforeMonday){
+          factory.changeCurrentJobId(thisJobId);
+          factory.editDate($scope.dateMonOne).then(function () {
+            $scope.monOneJobs.push(thisJob);
+          });
 
+        }
       }
     }
   };
 
   $scope.getAll = function () {
     console.log('in getall');
-    $scope.changeWeekStatus(0);
     //clear data
     $scope.alljobs = [];
     factory.getAll().then(function(results){
